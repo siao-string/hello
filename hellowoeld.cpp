75770520214c88f3b09cba6057d3a6276646480c
siao <siaojing@qq.com>
2022-01-11 12:10:26 +0800

    Test01

---
+++ hellowoeld.cpp
@@ -0,0 +1,9 @@
+//
+// Created by study on 2022/1/11.
+//
+
+#include "hellowoeld.h"
+
+int main(){
+    std::cout << "hello world" << std::endl;
+}
---
+++ hellowoeld.h
@@ -0,0 +1,15 @@
+//
+// Created by study on 2022/1/11.
+//
+
+#ifndef HELLO_HELLOWOELD_H
+#define HELLO_HELLOWOELD_H
+
+#include "iostream"
+
+class hellowoeld {
+
+};
+
+
+#endif //HELLO_HELLOWOELD_H
